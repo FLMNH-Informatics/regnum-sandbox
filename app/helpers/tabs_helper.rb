6b329996bbd4df0102248920a8a79e7cfa3f0d58
Chris Dell <cdell@ufl.edu>
2017-06-04 20:33:34 -0400

    Fixed comment: My Submissions:  “Create Submission” is a bit small and off to the side.  Could it be more prominently displayed (larger type and more centrally positioned)?  The three tabs and their associated text (Search, My Submissions, Logout) could also be larger.

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -20,17 +20,19 @@ module TabsHelper
   # map for holding display and linking behavior of tabs
   @@nav_menu = {
     :primary => {
-      :my_submission => {:text => "My Submissions", :controller => 'my_submission', :subnav => [ :cladename  ], :link => '/my_submission' },
+      :my_submission => {:text => "My Submissions", :controller => 'my_submission', :subnav => [ :my_submissions, :cladename], :link => '/my_submission' },
       :phenotypes => {:text => "Phenotypes", :controller => 'phenotypes', :subnav => [ ], :link => '/phenotypes' },
-      :submissions => { :text => "Review", :controller => 'submissions', :link => '/submissions' },
+      :submissions => { :text => "Review", :controller => 'submissions', :subnav => [ :review_submissions ], :link => '/submissions' },
       :search => { :text =>"Search", :controller => 'search', :subnav => [  ], :link => '/search'} ,
       :admin => { :text => 'Admin', :controller => 'admin', :subnav => [:list_users, :add_user], :link => '/admin'}
     },
     :secondary => {
-      :list_users => {:title => 'User List',:parent => :admin, :path => 'admin_path'} ,
-      :add_user => {:title => 'Add User', :parent => :admin, :path => 'add_user_admin_path'},
-      :cladename => { :title => "Create Submission",:subnav => [ :cladename , :definitiontype, :specifiers, :definition, :citation ], :parent => :my_submission, :path => 'new_submission_path' },
-      :accepted  => { :title => "Accepted Submissions", :parent=> :my_submission, :path => 'accepted_url' },
+      :review_submissions => { :title => "Review submissions", :parent => :submissions, :path => "submissions_path" },
+      :list_users =>      {:title => 'User List',:parent => :admin, :path => 'admin_path'} ,
+      :add_user =>        {:title => 'Add User', :parent => :admin, :path => 'add_user_admin_path'},
+      :my_submissions =>  {:title => 'Submissions', :parent => :my_submission, :path => 'my_submission_path'},
+      :cladename =>       { :title => "Create Submission",:subnav => [ :cladename , :definitiontype, :specifiers, :definition, :citation ], :parent => :my_submission, :path => 'new_submission_path' },
+      :accepted  =>       { :title => "Accepted Submissions", :parent=> :my_submission, :path => 'accepted_url' },
     }
     
   }
@@ -57,6 +59,14 @@ module TabsHelper
     end
     result_str <<  (logged_in? ? "<li class=\"top_tab active\"><a href=\"/logout\">Logout</a></li>" : "<li class=\"top_tab active\"><a href=\"/login\">Login</a></li>")
     result_str << "</ul></div>"
+  end
+
+  def current_subtab? subtab
+    return request.path == subtab_path_for(subtab)
+  end
+
+  def subtab_path_for subtab
+    return eval(@@nav_menu[:secondary][subtab][:path])
   end
 
   
@@ -95,7 +105,8 @@ module TabsHelper
             result_str << "<li class='top_subtab current'>#{link_to "#{@@nav_menu[:secondary][subtab][:title]}",   eval(@@nav_menu[:secondary][subtab][:path])}</li>"
             @sub_tab = subtab
           elsif controller_name == subtab.to_s
-            result_str << "<li class='top_subtab current'>#{link_to "#{@@nav_menu[:secondary][subtab][:title]}",   eval(@@nav_menu[:secondary][subtab][:path])}</li>"
+            byebug
+            result_str << "<li class='top_subtab current #{"active" if current_subtab?(subtab)}'>#{link_to "#{@@nav_menu[:secondary][subtab][:title]}",   eval(@@nav_menu[:secondary][subtab][:path])}</li>"
             @sub_tab = subtab
        
           elsif tab == :search && (controller.action_name == "display_species" or controller.action_name == "display_specimen" or controller.action_name == "display_apomorphy" or controller.action_name == "display_preexisting" or controller.action_name == "display_cladename")
@@ -116,7 +127,7 @@ module TabsHelper
             end
             result_str << "</ul>"
           else
-            result_str << "<li class='top_subtab active'>#{link_to raw("<span>#{@@nav_menu[:secondary][subtab][:title]}</span>"), eval(@@nav_menu[:secondary][subtab][:path])}</li>"
+            result_str << "<li class='top_subtab active#{" current" if current_subtab?(subtab)}'>#{link_to raw("<span>#{@@nav_menu[:secondary][subtab][:title]}</span>"), subtab_path_for(subtab)}</li>"
           end
         }
         result_str << "</ul>"
