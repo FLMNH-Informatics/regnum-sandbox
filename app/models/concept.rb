84a64e25210f7f9e002a8a23bc1fd42668df3811
Chris Dell <cdell@ufl.edu>
2018-04-11 00:10:05 -0400

    fixed deprecation of :order "terms" to -> { order terms }"

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -7,7 +7,7 @@ class Concept < ActiveRecord::Base
 
   belongs_to :ontology, :inverse_of => :concepts
 
-  has_many :terms, :inverse_of => :concept, :order => "term"#, :dependent => :delete_all
+  has_many :terms, :inverse_of => :concept, ->{ order "term"}#, :dependent => :delete_all
   has_one :term, :conditions => "preferred"
 
   has_many :relationships, :foreign_key => "from_concept_id", :inverse_of => :from_concept
